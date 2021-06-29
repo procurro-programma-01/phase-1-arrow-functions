4debdb4f79713a035e10db713153d4cc69333985
abraham saldivar <saldivara1010@gmail.com>
2021-06-29 17:07:24 -0500

    commit tree 989dfb2663f4ae9b0bf39abee5ec5b208480d84d

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
-/* // Traditional Function
-function (a){
-    return a + 100;
-  }
-  
-  // Arrow Function Break Down
-  
-  // 1. Remove the word "function" and place arrow between the argument and opening body bracket
-  (a) => {
-    return a + 100;
-  }
 
-Traditional Function
-function bob (a){
-    return a + 100;
-  }
-  
-  // Arrow Function
-  let bob = a => a + 100;
- */
-
-  
-          //  let divide =  (x , y ) => x / y  ; 
-            
-    
-     let divide = ( x = 2000 , y = 100) => x/y  ;
-      
-     
-     
-       
- // function square(x){
-     // return x * x 
-
- 
-
-const square = (x) => x * x ; 
-
- /*function add(x , y) {
-    x + y 
-    return x + y 
-}
-*/
-let add = ( x, y ) => x + y 
+let divide = (x = 2000, y = 100) => x / y 
 
 
 
+let square = (x = 2  , y = 2 ) => Math.pow(x , y)
+
+
+
+
+let add = (x , y ) => x + y
 
 
